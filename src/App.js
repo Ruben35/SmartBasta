aafda7655e2d2e71364acc51d53cb1da373e52b6
Ruben35 <ruben111416@gmail.com>
2021-05-24 15:41:18 -0500

    Partial ActualPuntuation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import SimpleNavbar from './components/SimpleNavbar'
 import { useState } from 'react';
+import ActualPuntuation from './components/ActualPuntuation';
 import GameNavbar from './components/GameNavbar'
 import PlayersSideBar from './components/PlayersSideBar'
 
@@ -15,6 +16,10 @@ function App() {
       <section className="gameContainer">
         <PlayersSideBar open={playersBarOpen} onClose={handleOnClickPlayers}/>
         <GameNavbar type="stop" onClickPlayers={handleOnClickPlayers} />
+        <div className="mainGameScreen">
+          hola
+          <ActualPuntuation/>
+        </div>
       </section>
     </>
   );
---
+++ src/components/ActualPuntuation.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const ActualPuntuation = () =>{
+    return(
+        <div className="actualPuntuationBar">
+            bootom
+            <div> {/*The rectangle*/}
+                <div> 
+                    <span>Puntuación</span>
+                    <span>350pts</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ActualPuntuation;
